dee39804611b4504d23e89f32673c836a0d020fa
Jose Fuentes <jafcalvente@outlook.com>
2016-08-19 23:09:52 +0200

    Añadido filtrado por nombre en el listado.

--- js/script.js
+++ js/script.js
@@ -118,9 +118,48 @@ app.controller("MainController", ["$scope", function($scope) {
 
 }]);
 
+// Definimos un filtro que permita filtrar por texto introducido
+app.filter("filteri18n", ["$filter", function($filter) {
+  var filterFn = $filter("filter");
+
+  /** Transforma el texto quitando todos los acentos diéresis, etc. **/
+  function normalize(texto) {
+    texto = texto.replace(/[áàäâ]/g, "a");
+    texto = texto.replace(/[éèëê]/g, "e");
+    texto = texto.replace(/[íìïî]/g, "i");
+    texto = texto.replace(/[óòôö]/g, "o");
+    texto = texto.replace(/[úùüü]/g, "u");
+    texto = texto.toUpperCase();
+    return texto;
+  }
+
+  /** Esta función es el comparator en el filter **/
+  function comparator(actual, expected) {
+      if (normalize(actual).indexOf(normalize(expected)) >= 0) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  /** Este es realmente el filtro **/
+  function filteri18n(array, expression) {
+
+    //Lo único que hace es llamar al filter original pero pasado
+    //la nueva función de comparator
+    return filterFn(array, expression, comparator)
+  }
+
+  return filteri18n;
+}]);
+
 // Controlador para la página del listado
 app.controller("ListadoSegurosController", ["$scope", "seguros", function($scope, seguros) {
   $scope.seguros = seguros;
+
+  $scope.filtro = {
+    nombre: ""
+  };
 }]);
 
 // Controlador para la página de detalles
